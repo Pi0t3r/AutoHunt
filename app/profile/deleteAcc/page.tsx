a51a51dd4b6442d28ba555fa7b4db696ecd68e97
Piotr Kapuściński <kapuscinskipiotr5@gmail.com>
2023-09-03 02:01:53 +0200

    add a posibility to current login user to delete account

--- app/profile/deleteAcc/page.tsx
+++ app/profile/deleteAcc/page.tsx
@@ -1,16 +1,33 @@
 "use client";
-
 import React from "react";
 import Link from "next/link";
-import DeleteUser from "@/components/delete/Delete";
+import { auth, db } from "@/firebase";
+import { useUserContext } from "@/context/UserContext";
+import { deleteDoc, doc } from "firebase/firestore";
 export default function DeleteAcc() {
+  const { user, setUser } = useUserContext();
+  async function handleDeleteUser() {
+    const user = auth.currentUser;
+
+    try {
+      const userId = user?.uid;
+      await deleteDoc(doc(db, "users", userId));
+      await user?.delete();
+      await auth.signOut();
+      setUser(null);
+      console.log("User Account deleted succesful!");
+      window.location.href = "/";
+    } catch (err) {
+      console.error("Error with deleting user: ", err);
+    }
+  }
   return (
     <div>
       <Link href="/profile">
         <button>Back</button>
       </Link>
       <h2>Are you sure?</h2>
-      <button onClick={DeleteUser}>Yes, delete my account</button>
+      <button onClick={handleDeleteUser}>Yes, delete my account</button>
     </div>
   );
 }
